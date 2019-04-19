d2c79ed1069fc2dc39a6772484ce4eb0d9ad7228
Toto Rubianto Toto Rubianto <toto.rubianto.17@gmail.com>
2019-04-19 13:04:40 +0700

    instalasi

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/core/Navbar";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <nuxt-link to="/auth/login">login</nuxt-link>
+    <nuxt-link to="/auth/signup">signup</nuxt-link>
+  </section>
+</template>
+
+<script>
+import BLogo from '@/components/Logo'
+
+export default {
+  name: 'HomePage',
+}
+</script>
