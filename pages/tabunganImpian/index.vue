f96c0b7fa763a228920dfe0c6f4f203df43c908d
Kharis Azhar <muhammadkharisazhar149a@gmail.com>
2019-04-20 18:47:26 +0700

    fix menu tabungan impian

--- pages/tabunganImpian/index.vue
+++ pages/tabunganImpian/index.vue
@@ -6,7 +6,7 @@
         <div class="column is-4">
           <div class="card padding-30 has-text-centered">
             <img src="~/assets/img/icons/dream.png" alt>
-            <div class="">
+            <div class>
               <p class="title is-4">Tabungan Impian</p>
               <p class="subtitle is-6">Rencanakan Impianmu Bersama Kami</p>
             </div>
@@ -18,17 +18,45 @@
               <div class="columns">
                 <div class="column">
                   <p class="title is-4">Toto Rubianto</p>
+                </div>
+                <div class="column is-7">
+                  <div class="is-pulled-right">
+                    <span>Saldo</span>
+                    <h1 class="title is-4 inline">
+                      IDR
+                      <span class="has-text-green">3.000.000</span>
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
                   <p class="is-6">No Rekening : 0989798729998</p>
                 </div>
-                <div class="column">
-                  <label>Saldo</label>
+                <div class="column is-7">
+                  <div class="is-pulled-right">
+                    <span>Hold Amount</span>
+                    <h1 class="title is-4 inline">
+                      IDR
+                      <span class="has-text-green">0</span>
+                    </h1>
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div class="padding-left-right-30 background-green">Periode 01/04/2019</div>
+            <div class="padding-left-right-30 background-green">
+              <div style="display:inline;padding-right:104px;">Periode 01/04/2019</div>
+              <div style="display:inline;">Hingga 02/04/2019</div>
+            </div>
+
             <div class="padding-30">
               <b-table :data="data" :columns="columns"></b-table>
+            </div>
+            <div class="card-footer">
+              <div class="card-footer-item">
+                <nuxt-link class="title is-6" v-bind:to="''" >Lihat Selengkapnya</nuxt-link>
+              </div>
             </div>
           </div>
         </div>
