798357a248981b87585274081235760c100cb8e5
Toto Rubianto Toto Rubianto <toto.rubianto.17@gmail.com>
2019-04-20 17:25:39 +0700

    dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,19 +1,54 @@
 <template>
   <div>
     <div class="columns is-multiline">
-            <div class="column is-4">
-              <Card/>
+      <div v-for="item in investment" v-bind:key="item.id" class="column is-4">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img :src="require('@/assets/img/'+item.img)" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <h5 class="title is-6">{{item.nama}}</h5>
+            <p class="is-size-6 is-vertical-center">
+              <span class="padding-right-11">
+                <img src="~/assets/img/icons/location.svg" alt="loct">
+              </span>
+              {{item.alamat}}
+            </p>
+            <p class="is-size-6">
+              <span class="padding-right-11">
+                <img src="~/assets/img/icons/store.svg" alt="store">
+              </span>
+              {{item.jenis}}
+            </p>
+            <p class="is-size-6">
+              <span class="padding-right-8">
+                <img src="~/assets/img/icons/discount.svg" alt="disc">
+              </span>
+              Keuntungan 15%
+            </p>
+            <div class="progress-wrapper padding-top-22">
+              <progress class="progress is-danger is-small" :value="item.progres" max="100">90%</progress>
             </div>
-            <div class="column is-4">
-              <Card/>
-            </div>
-            <div class="column is-4">
-              <Card/>
-            </div>
-            <div class="column is-4">
-              <Card/>
+            <p class="title has-text-grey padding-top-8 is-6">Terkumpul</p>
+            <p class="subtitle is-6">Rp. 50.230.000</p>
+            <div class="control margin-top-22">
+              <nuxt-link class="button is-danger is-fullwidth is-outlined" v-bind:to="'/dashboard/' + item.id" >Invest</nuxt-link>
+              
             </div>
           </div>
+        </div>
+      </div>
+
+      <!--
+            <div class="column is-4">
+              <Card/>
+            </div>
+            <div class="column is-4">
+              <Card/>
+      </div>-->
+    </div>
   </div>
 </template>
 
@@ -21,8 +56,45 @@
 import Card from "@/components/dashboard/card";
 export default {
   layout: "dashboard",
+  data() {
+    return {
+      
+      investment: [
+        {
+          id: 1,
+          nama: "Perumahan Teluk",
+          alamat: "Jakarta Barat, Jl Angsana III",
+          jenis: "Rumah",
+          keuntungan: "Kuntungan 5%",
+          progres: 80,
+          dana_terkumpul: 45,
+          img:"home1.jpg"
+        },
+        {
+           id: 2,
+          nama: "Perumahan Sokaraja",
+          alamat: "Sokaraja, Jl Angsana III no 90",
+          jenis: "Rumah",
+          keuntungan: "Kuntungan 5%",
+          progres: 45,
+          dana_terkumpul: 45,
+          img:"home2.jpg"
+        },
+        {
+          id: 3,
+          nama: "Kantor Telkom",
+          alamat: "Jakarta Selatan, Jl Purwokerto III",
+          jenis: "Rumah",
+          keuntungan: "Kuntungan 5%",
+          progres: 45,
+          dana_terkumpul: 45,
+          img:"home3.jpeg"
+        }
+      ]
+    };
+  },
   components: {
-    Card,
+    Card
   }
 };
 </script>
--- pages/tabunganImpian/index.vue
+++ pages/tabunganImpian/index.vue
@@ -4,12 +4,35 @@
     <div class="container relative top-300">
       <div class="columns">
         <div class="column is-4">
-          <div class="card padding-30">
-            <h1>asdasd</h1>
+          <div class="card padding-30 has-text-centered">
+            <img src="~/assets/img/icons/dream.png" alt>
+            <div class="">
+              <p class="title is-4">Tabungan Impian</p>
+              <p class="subtitle is-6">Rencanakan Impianmu Bersama Kami</p>
+            </div>
+          </div>
+        </div>
+        <div class="column is-8">
+          <div class="card">
+            <div class="padding-30">
+              <div class="columns">
+                <div class="column">
+                  <p class="title is-4">Toto Rubianto</p>
+                  <p class="is-6">No Rekening : 0989798729998</p>
+                </div>
+                <div class="column">
+                  <label>Saldo</label>
+                </div>
+              </div>
+            </div>
+
+            <div class="padding-left-right-30 background-green">Periode 01/04/2019</div>
+            <div class="padding-30">
+              <b-table :data="data" :columns="columns"></b-table>
+            </div>
           </div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
@@ -19,10 +42,63 @@ import Hero from "@/components/tabungan/tabunganImpian/hero";
 export default {
   components: {
     Hero
+  },
+  data() {
+    return {
+      data: [
+        {
+          tanggal: "2016-10-15",
+          transaksi: "Potongan kebutuhan SPP/ Bulan Potongan Kebutuhan Listrik",
+          debit: "-",
+          credit: "100000"
+        },
+        {
+          tanggal: "2016-10-15",
+          transaksi: "Potongan kebutuhan SPP/ Bulan Potongan Kebutuhan Listrik",
+          debit: "-",
+          credit: "100000"
+        },
+        {
+          tanggal: "2016-10-15",
+          transaksi: "Potongan kebutuhan SPP/ Bulan Potongan Kebutuhan Listrik",
+          debit: "100000",
+          credit: "-"
+        },
+        {
+          tanggal: "2016-10-15",
+          transaksi: "Potongan kebutuhan SPP/ Bulan Potongan Kebutuhan Listrik",
+          debit: "-",
+          credit: "100000"
+        },
+        {
+          tanggal: "2016-10-15",
+          transaksi: "Potongan kebutuhan SPP/ Bulan Potongan Kebutuhan Listrik",
+          debit: "100000",
+          credit: "-"
+        }
+      ],
+      columns: [
+        {
+          field: "tanggal",
+          label: "Tanggal"
+        },
+        {
+          field: "transaksi",
+          label: "Transaksi"
+        },
+        {
+          field: "debit",
+          label: "Debit"
+        },
+        {
+          field: "credit",
+          label: "Credit"
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style>
-
 </style>
