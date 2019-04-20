78588f3560e9071d56ee867f5560e860e83380c9
Toto Rubianto Toto Rubianto <toto.rubianto.17@gmail.com>
2019-04-20 13:40:24 +0700

    componets tabungan

---
+++ components/tabungan/hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section class="hero has-bg-img is-medium ">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title has-text-white">Investasi bersama Simavest</h1>
+          <h2 class="subtitle has-text-white">Medium subtitle</h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.has-bg-img {
+  background-image: linear-gradient(264deg, #ff8225, #ef3088);
+  background-size: cover;
+}
+</style>
