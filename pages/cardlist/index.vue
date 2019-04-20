529d2fefb4ec05e4585e19ade3eb083cc73dc27d
Kharis Azhar <muhammadkharisazhar149a@gmail.com>
2019-04-20 11:41:03 +0700

    card horizontal list

--- pages/cardlist/index.vue
+++ pages/cardlist/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="card-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eum aperiam temporibus necessitatibus velit tempora vitae exercitationem expedita quia facere. Atque eveniet voluptatum consequatur minima ipsam quos eos pariatur dolores?
+    <div class="card is-horizontal">
+      <div class="columns">
+        <div class="column is-3">
+          <div class="card-image">
+            <figure class="image is-128x128">
+              <img class="width-100" src="https://bulma.io/images/placeholders/256x256.png">
+            </figure>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card-content">
+            <h5 class="title is-6">Green Taman Sutra</h5>
+            <p class="is-size-6 is-vertical-center">
+              <span class="padding-right-11">
+                <img src="~assets/img/icons/location.svg" alt="loct">
+              </span>
+              Jakarta Barat, Jl Angsana III
+            </p>
+            <p class="is-size-6">
+              <span class="padding-right-11">
+                <img src="~assets/img/icons/store.svg" alt="store">
+              </span>
+              Rumah
+            </p>
+            <p class="is-size-6">
+              <span class="padding-right-8">
+                <img src="~assets/img/icons/discount.svg" alt="disc">
+              </span>
+              Keuntungan 15%
+            </p>
+            <div class="progress-wrapper padding-top-22">
+              <progress class="progress is-danger is-small" value="90" max="100">90%</progress>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,5 +46,36 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.card .media:not(:last-child) {
+  margin-bottom: 0.75rem;
+}
+.card.card--reverse-order {
+  flex-direction: row-reverse;
+}
+.card.is-horizontal {
+  display: flex;
+  flex-basis: 50ex;
+  flex-grow: 0;
+  flex-shrink: 1;
+}
+.card.is-horizontal .card-image {
+  flex: 4;
+  flex-shrink: 1;
+}
+.card.is-horizontal .card-image .image {
+  display: block;
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+}
+.card.is-horizontal .card-image .image img {
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+.card.is-horizontal .card-content {
+  flex: 3;
+}
 </style>
